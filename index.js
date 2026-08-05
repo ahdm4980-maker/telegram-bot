9c75e33205eb1e3b7d4e43388d579f3a29bd3dae
ahdm4980-maker <ahdm4980@gmail.com>
2026-08-05 06:37:59 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token="8849951874:AAEvrzdVgfuWF2BoKopAJ-HTSWjXDngqhr4";
+
+const token =8849951874:AAEvrzdVgfuWF2BoKopAJ-HTSWjXDngqhr4;
+
 const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "سلام! ربات با موفقیت روشن شد. 🎉");
+});
