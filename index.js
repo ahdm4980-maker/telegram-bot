0cc1964c998552816d5c769ad23897612564d76d
ahdm4980-maker <ahdm4980@gmail.com>
2026-08-05 06:33:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const token = "8849951874:AAEvrzdVgfuWF2BoKopAJ-HTSWjXDngqhr4";
+const token="8849951874:AAEvrzdVgfuWF2BoKopAJ-HTSWjXDngqhr4";
 
 const bot = new TelegramBot(token, { polling: true });
